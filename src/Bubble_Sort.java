23b81ac37a7c745d38a7236ebdaf9015ef5bc2c7
ajinkyasultane <ajinkyasultane@gmail.com>
2025-04-16 18:25:32 +0530

    Bubble sort Algorithm Normal Changes

--- src/Bubble_Sort.java
+++ src/Bubble_Sort.java
@@ -9,24 +9,24 @@ public class Bubble_Sort {
         System.out.println(Arrays.toString(arr));
     }
 
+
+
     static void bubblesort(int[] arr)
     {
-        //run the step n- 1 times
-        for (int i = 0 ; i < arr.length; i++)
+        for(int i = 0; i < arr.length; i++)
         {
-            // for each step , max item will come to last respective index
-            for(int j = 1 ; j < arr.length - i ; j++)
+            for (int j = 1 ; j < arr.length-i ; j++)
             {
-                if(arr[j] < arr[j-1])
+                if(arr[j] < arr[j - 1])
                 {
                     int temp = arr[j];
                     arr[j] = arr[j - 1];
-                    arr[j - 1] = temp ;
+                    arr[j - 1 ] = temp ;
                 }
             }
         }
-
     }
+
 
 
 }
